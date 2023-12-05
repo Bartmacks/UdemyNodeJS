f3ac31539dc6cd360b3180b150a84a42a2772ef3
D Bender <33201183+Bartmacks@users.noreply.github.com>
2023-12-05 21:49:06 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,34 @@ const getNotes = require("./notes.js");
 
 yargs.command({
   command: "add",
-  describe: "Add a new note",
+  describe: "Add a new note.",
   handler: function () {
     console.log("Adding a new note!");
   },
 });
+
+yargs.command({
+  command: "remove",
+  describe: "Removing a note.",
+  handler: function () {
+    console.log("Removing the note.");
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "List your notes.",
+  handler: function () {
+    console.log("Listing out all notes.");
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe: "Reading a note",
+  handler: function () {
+    console.log("Reading the note.");
+  },
+});
+
 console.log(yargs.argv);

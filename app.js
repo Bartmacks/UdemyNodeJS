5cb3ed42c40867d4db9b68c1c7338f7c275d8ddb
D Bender <33201183+Bartmacks@users.noreply.github.com>
2023-12-05 21:38:26 +0000

    Update 2 files

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-const yargs = import("yargs");
-const getNotes = require(./notes.js);
+const yargs = require("yargs");
+const getNotes = require("./notes.js");
+
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding a new note!");
+  },
+});
+console.log(yargs.argv);
